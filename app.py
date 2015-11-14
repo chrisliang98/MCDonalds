8c589dbcf9801dd5ac43dda3922b5cab06c4ac70
Chris Liang <chrisliang98@gmail.com>
2015-11-14 15:50:30 -0500

    integrated flickr api

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
 import urllib2,json
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import flickr_api
+
+flickr_key = "dca00d0c7782247d89a98c1ee617a0ee"
+flickr_secret = "5b568623184f70ca"
+
+secrets = {'api_key' : flickr_key, 'api_secret' : flickr_secret }
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("home.html");
+@app.route("/t")
+@app.route("/t/<tag>")
+def t(tag):
+    flickr_api.set_keys(**secrets)
+    photos = flickr_api.Photo.search(tags=tag, sort='date-posted-desc', per_page=10)
+    j = []
+    for photo in photos:
+        j.append(
+            {
+                'title': photo.title,
+                'description': photo.title,
+                'url': photo.getPhotoUrl()
+            }
+        )
+    response = Response(json.dumps({'photos': j}), status=200, mimetype='application/json')
+    
+#works to get the info, now need to get direct url to pic and put that into a render_template return 
+    #photos[]
+
+    #for r in response:
+     #   try:    
+      #      newPhoto = r[]
+
+    return response
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.debug = True
-    app.run(host"0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
 
